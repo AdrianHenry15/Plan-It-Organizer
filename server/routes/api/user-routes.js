699bee86ef9e87a1fcf3c3d5774378cfb670403e
acamp <acampbell8610@gmail.com>
2022-05-20 13:20:23 -0500

    server is running, cleaned up code and added seeds

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,22 +1,22 @@
-const router = require('express').Router();
-const {
-    createUser,
-    getSingleUser,
-    savePlan,
-    deletePlan,
-    login,
-} = require('../../controllers/user-controller');
+// const router = require('express').Router();
+// const {
+//     createUser,
+//     getSingleUser,
+//     savePlan,
+//     deletePlan,
+//     login,
+// } = require('../../controllers/user-controller');
 
-// import middleware
-const { authMiddleware } = require('../../utils/auth');
+// // import middleware
+// const { authMiddleware } = require('../../utils/auth');
 
-// put authMiddleware anywhere we need to send a token for verification of user
-router.route('/').post(createUser).put(authMiddleware, savePlan);
+// // put authMiddleware anywhere we need to send a token for verification of user
+// router.route('/').post(createUser).put(authMiddleware, savePlan);
 
-router.route('/login').post(login);
+// router.route('/login').post(login);
 
-router.route('/me').get(authMiddleware, getSingleUser);
+// router.route('/me').get(authMiddleware, getSingleUser);
 
-router.route('/plans/:planId').delete(authMiddleware, deletePlan);
+// router.route('/plans/:planId').delete(authMiddleware, deletePlan);
 
-module.exports = router;
+// module.exports = router;
