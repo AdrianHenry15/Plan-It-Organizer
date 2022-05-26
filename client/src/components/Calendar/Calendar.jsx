e618650c5e71587c0f035bb5611744d7deb6e480
Adrian <adrianhenry2115@gmail.com>
2022-05-26 19:00:13 -0400

    static calendar added

--- client/src/components/Calendar/Calendar.jsx
+++ client/src/components/Calendar/Calendar.jsx
@@ -1,17 +1,18 @@
-import React, {useState} from 'react';
-import ReactCalendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-
+import React from 'react';
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import timeGridPlugin from '@fullcalendar/timegrid'
 
 const Calendar = () => {
-
-    const [value, onChange] = useState(new Date());
-
   return (
     <div>
-      <ReactCalendar onChange={onChange} value={value} />
+      <FullCalendar
+        plugins={[ dayGridPlugin, timeGridPlugin ]}
+        
+      />
     </div>
-  );
+  )
 }
 
 export default Calendar
+
