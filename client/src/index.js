2ef7f6292d18651c03608920cd71cb338d1c0532
Adrian <adrianhenry2115@gmail.com>
2022-05-28 22:50:54 -0400

    pizel art image saved

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './App.css';
 import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
