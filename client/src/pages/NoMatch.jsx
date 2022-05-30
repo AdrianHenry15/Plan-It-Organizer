d1f994cf25f941cada1cf5e6bbaa335b2647f262
Adrian <adrianhenry2115@gmail.com>
2022-05-30 08:34:49 -0400

    404 page update

--- client/src/pages/NoMatch.jsx
+++ client/src/pages/NoMatch.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const NoMatch = () => {
   return (
-    <div>
+    <div className='text-center text-7xl tracking-wide py-20 '>
       <h1>404</h1>
-      <p>the page you are looking for doesn't exist</p>
+      <p>The Page You Are Looking For</p>
+      <p> Does Not Exist</p>
     </div>
   )
 }
