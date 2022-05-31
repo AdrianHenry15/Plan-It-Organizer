588dd17b4d2a45ee8483c4369c4c504e789978ca
jaredpennington <94023319+jaredpennington@users.noreply.github.com>
2022-05-30 20:26:43 -0400

    styling changes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -35,12 +35,12 @@ const Login = () => {
   return (
     <div className=''>
       <div className='flex h-screen justify-center items-center'>
-        <div className='bg-gradient-to-b from-bubblegum-400 to-cream-400 rounded-lg mx-28 w-60 py-2'>
+        <div className='bg-gradient-to-b from-bubblegum-400 to-cream-400 rounded-lg mx-28  w-80 py-2'>
           <h4 className='text-center text-black text-2xl my-6'>Login</h4>
           <div className=''>
             <form className='text-slate-900 flex flex-col' onSubmit={handleFormSubmit}>
               <input
-                className='w-32 my-1 mx-auto align-middle rounded-sm px-1'
+                className='w-10/16 my-1 mx-auto align-middle rounded-sm px-1'
                 placeholder='Your email'
                 name='email'
                 type='email'
@@ -49,7 +49,7 @@ const Login = () => {
                 onChange={handleChange}
               />
               <input
-                className='w-32 mx-auto my-1 rounded-sm px-1'
+                className='w-10/16 mx-auto my-1 rounded-sm px-1'
                 placeholder='******'
                 name='password'
                 type='password'
@@ -57,7 +57,7 @@ const Login = () => {
                 value={formState.password}
                 onChange={handleChange}
               />
-              <Link className='mx-auto text-center text-blue-600 hover:text-sky-300 transition-all duration-300 mx-auto p-2 mx-2 rounded-md' to="/signup">
+              <Link className='mx-auto text-center text-purple-700 hover:text-purple-400 transition-all duration-300 mx-auto p-2 mx-2 rounded-md' to="/signup">
                 Don't have an account?
               
               </Link>
